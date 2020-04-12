8a95675f58fae33a2d03287ebe5b2b0c9130aeea
Kamahin Mihail <kamahinmihail@gmail.com>
2020-04-12 19:32:25 +0300

    123

--- script/script.js
+++ script/script.js
@@ -1,19 +1,26 @@
-var rect1 = {
-    id: '1234',
-    size: {
-        width: 20,
-        height: 20
-    },
-    color: '#ccc'
-};
-var rect2 = {
-    id: '12345',
-    size: {
-        width: 10,
-        height: 10
-    }
-};
-rect2.color = '#000';
-var rect3 = {};
-var rect4 = {};
-console.log(1);
+// interface Rect {
+// 	readonly id: string,
+// 	color?: string,
+// 	size: {
+// 		width: number,
+// 		height: number
+// 	}
+// }
+// const rect1: Rect = {
+// 	id: '1234',
+// 	size: {
+// 		width: 20,
+// 		height: 20
+// 	},
+// 	color: '#ccc'
+// };
+// const rect2: Rect = {
+// 	id: '12345',
+// 	size: {
+// 		width: 10,
+// 		height: 10
+// 	}
+// };
+// rect2.color = '#000';
+// const rect3 = {} as Rect;
+// const rect4 = <Rect>{};
--- script/script.ts
+++ script/script.ts
@@ -1,30 +1,30 @@
-interface Rect {
-	readonly id: string,
-	color?: string,
-	size: {
-		width: number,
-		height: number
-	}
-}
+// interface Rect {
+// 	readonly id: string,
+// 	color?: string,
+// 	size: {
+// 		width: number,
+// 		height: number
+// 	}
+// }
 
-const rect1: Rect = {
-	id: '1234',
-	size: {
-		width: 20,
-		height: 20
-	},
-	color: '#ccc'
-};
+// const rect1: Rect = {
+// 	id: '1234',
+// 	size: {
+// 		width: 20,
+// 		height: 20
+// 	},
+// 	color: '#ccc'
+// };
 
-const rect2: Rect = {
-	id: '12345',
-	size: {
-		width: 10,
-		height: 10
-	}
-};
+// const rect2: Rect = {
+// 	id: '12345',
+// 	size: {
+// 		width: 10,
+// 		height: 10
+// 	}
+// };
 
-rect2.color = '#000';
+// rect2.color = '#000';
 
-const rect3 = {} as Rect;
-const rect4 = <Rect>{};
+// const rect3 = {} as Rect;
+// const rect4 = <Rect>{};

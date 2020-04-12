b3e10c13fe104adc141e2dcb3773143d76300af2
Kamahin Mihail <45814283+mihinov@users.noreply.github.com>
2020-04-12 19:04:24 +0300

    123

---
+++ script/script.js
@@ -0,0 +1,19 @@
+var rect1 = {
+    id: '1234',
+    size: {
+        width: 20,
+        height: 20
+    },
+    color: '#ccc'
+};
+var rect2 = {
+    id: '12345',
+    size: {
+        width: 10,
+        height: 10
+    }
+};
+rect2.color = '#000';
+var rect3 = {};
+var rect4 = {};
+console.log(1);
